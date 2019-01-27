5fb82835440e44e5d477796a24b0ea10d1ee68f3
Li-Ying Liang <ryou_u@msn.com>
2019-01-28 02:50:41 +0800

    thread operator

---
+++ LYLStudio.Core/Threading/IAnything.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LYLStudio.Core.Threading
+{
+    public interface IAnything<T>
+    {
+        Action<T> Callback { get; }
+        T Parameters { get; }
+    }      
+}
---
+++ LYLStudio.Core/Threading/OperatorEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LYLStudio.Core.Threading
+{
+    public class OperatorEventArgs : EventArgsBase
+    {
+        public OperatorEventArgs() : base()
+        {
+            
+        }
+
+        public OperatorEventArgs(IResult result) : base(result)
+        {            
+        }
+    }
+}
