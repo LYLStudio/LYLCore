98f8556e9dd05894fa1c00f6c3276c4512f5cc20
Li-Ying Liang <ryou_u@msn.com>
2019-02-08 23:55:22 +0800

    inode todo later

--- LYLStudio.Core/Structures/INode.cs
+++ LYLStudio.Core/Structures/INode.cs
@@ -15,27 +15,29 @@ namespace LYLStudio.Core.Structures
         T Data { get; set; }
         bool HasChildren { get; }
         int Count { get; }
+
+        INode<T> FindNode(string id);
     }
 
-    public class Node<T> : INode<T>
-    {
-        public delegate bool TraversalNodeDelegate(INode<T> node);
+    //public class Node<T> : INode<T>
+    //{
+    //    public delegate bool TraversalNodeDelegate(INode<T> node);
 
-        private IList<INode<T>> _children { get; }
-
-
-        public string Id { get; set; }
-        public string ParentId { get; set; }
-        public T Data { get; set; }
-        public bool HasChildren => _children.Count > 0;
-        public int Count => _children.Count;
-
-        public Node(T data = default(T))
-        {
-            _children = new List<INode<T>>();
-            Data = data;
-        }
+    //    private IList<INode<T>> _children { get; }
 
 
-    }
+    //    public string Id { get; set; }
+    //    public string ParentId { get; set; }
+    //    public T Data { get; set; }
+    //    public bool HasChildren => _children.Count > 0;
+    //    public int Count => _children.Count;
+
+    //    public Node(T data = default(T))
+    //    {
+    //        _children = new List<INode<T>>();
+    //        Data = data;
+    //    }
+
+
+    //}
 }
